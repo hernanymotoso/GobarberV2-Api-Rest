a75e85d55d898398b8368d9e4337dcd88439fe43
Hernany <contatohernany@gmail.com>
2019-11-15 16:49:43 -0300

    Passei as configurações do DB para o config/database.js  e implementei(configurei) o sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '198.168.99.100',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
