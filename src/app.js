04a371c2c72306c852e8348a2bf5e618e025dd9f
Hernany <contatohernany@gmail.com>
2019-11-16 14:34:10 -0300

    Criei o ProviderController e a feature de listagem de prestadores de serviços(index)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 import './database';
 class App {
@@ -11,8 +12,8 @@ class App {
   }
 
   middlewares() {
-
     this.server.use(express.json());
+    this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
   }
 
   routes() {
