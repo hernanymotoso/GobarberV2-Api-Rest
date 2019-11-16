f878df644f11b9a9203e8e9bd34217e7f7997ea5
Hernany <contatohernany@gmail.com>
2019-11-16 20:29:56 -0300

    implementei o nodemailer, fiz as configurações no config/mail.js e no lib/Mail.js e implementei a funcionalidade de enviar email de cancelamento para o usuario

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "9235ba3f2e2f19",
+    pass: "9696ded952d457"
+  },
+  default: {
+    from: 'Equipe gobarver-v2 <noreply@gobarner.com>',
+  },
+}
